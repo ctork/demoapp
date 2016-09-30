0cb78197acc680f9ff249bbe152cf5bdf68520ef
Claire Torkington <clairetorkington@gmail.com>
2016-09-30 01:51:15 +0000

    added home help tests

--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticpagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+  assert_select "hl", "demoApp" 
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "hl" "help"
   end
 
 end
